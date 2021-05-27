b04adba28bd36b6641bdfccd9a992e67ebb61471
Juan Carlos <elfkampher@gmail.com>
2021-05-27 13:48:09 -0500

    cambio de nombre de empresa

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -9,7 +9,7 @@ session_start();
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   
-  <title>SymbioTICs | SinapSys</title>
+  <title>encodyne | SinapSys 2.0</title>
 
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
